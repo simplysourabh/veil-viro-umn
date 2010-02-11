249d58467d1669dff71ebac1da4c28aab957f2e3
gowricp <gowricp@7ed3ea50-16bc-11df-9d3e-97c80f6cbf9d>
2010-02-11 03:26:42 +0000

    header for veil hello packet generator

---
+++ veil/generatehello.hh
@@ -0,0 +1,37 @@
+#ifndef CLICK_VEILGENERATEHELLO_HH
+#define CLICK_VEILGENERATEHELLO_HH
+#include <click/element.hh>
+#include <click/timer.hh>
+#include "click_veil.hh"
+//#include "veil_hosttable.hh"
+//#include "veil_generatevid.hh"
+ 
+CLICK_DECLS
+ 
+class VEILGenerateHello : public Element {
+	public:
+ 
+		VEILGenerateHello();
+		~VEILGenerateHello();
+ 
+		const char *class_name() const { return "VEILGenerateHello"; }
+		const char *port_count() const { return PORTS_0_1; }
+		const char *processing() const { return PUSH; }
+ 
+		int configure(Vector<String> &, ErrorHandler *);
+		int initialize(ErrorHandler *);
+
+		void run_timer(Timer *);
+
+	private:
+		Timer myTimer;
+		//VID myVid;
+		//EtherAddress myMac;
+		EtherAddress myVid;
+		//VEILGenerateVid *genVid;
+		//VEILHostTable *hosts;
+
+};
+ 
+CLICK_ENDDECLS
+#endif
