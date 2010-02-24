f65a68b837cef10cce41ec7fb4504471c2d06207
gowricp <gowricp@7ed3ea50-16bc-11df-9d3e-97c80f6cbf9d>
2010-02-24 00:45:05 +0000

    corresponding header

---
+++ veil/processarp.hh
@@ -0,0 +1,37 @@
+#ifndef CLICK_VEILPROCESSARPREQUEST_HH
+#define CLICK_VEILPROCESSARPREQUEST_HH
+#include <click/element.hh>
+#include <click/timer.hh>
+#include <click/hashmap.hh>
+#include "hosttable.hh"
+#include "mappingtable.hh"
+
+CLICK_DECLS
+
+class VEILProcessARP: public Element {
+	public:
+		VEILProcessARP();
+		~VEILProcessARP();
+
+		const char* class_name() const { return "VEILProcessARPRequest"; }
+		const char* port_count() const { return PORTS_1_1; } 
+		//const char* processing() const { return PUSH; }
+		const char* processing() const { return "a/h"; }
+
+		int configure(Vector<String>&, ErrorHandler*);
+		Packet* smaction(Packet*);
+	 	virtual void push (int, Packet*);
+
+	private:
+		//EtherAddress myMac;
+		VID myVid;
+
+		VEILHostTable *host_table;
+		VEILMappingTable *map;
+		//VEILAccessNode accessNode;
+		//VEILGenerateVid *genVid;
+			
+};
+
+CLICK_ENDDECLS
+#endif
