22b23ec8a0e01ff78258f7804e09517fd3740d08
gowricp <gowricp@7ed3ea50-16bc-11df-9d3e-97c80f6cbf9d>
2010-02-24 00:44:22 +0000

    corresponding header

---
+++ veil/mappingtable.hh
@@ -0,0 +1,47 @@
+#ifndef CLICK_VEILIPMAPPINGTABLE_HH
+#define CLICK_VEILIPMAPPINGTABLE_HH
+
+#include <click/element.hh>
+#include <click/timer.hh>
+#include <click/hashtable.hh>
+#include "click_veil.hh"
+
+CLICK_DECLS
+
+//typedef HashTable<VID, IPAddress> ReverseIPMapTable; //do i need this?
+
+class VEILMappingTable : public Element {
+	public:
+		VEILMappingTable();
+		~VEILMappingTable();
+
+		const char* class_name() const { return "VEILIPMappingTable"; }
+		const char* port_count() const { return PORTS_0_0; }
+		const char* processing() const { return PUSH; }
+
+		//int configure(Vector<String>&, ErrorHandler*);
+		void updateEntry(IPAddress*, VID*, VID*);
+		bool lookupIP(IPAddress*, VID*, VID*);		
+		static void expire(Timer*, void*);
+		static String read_handler(Element*, void*);
+		void add_handlers();
+
+	private:
+		struct MappingTableEntry {
+			VID ipVid;
+			VID myVid;
+			Timer *expiry;
+		};
+		
+		typedef HashTable<IPAddress, MappingTableEntry> IPMapTable;
+		IPMapTable ipmap;
+
+		struct TimerData {
+			IPMapTable *ipmap;
+			IPAddress* ip;
+		};
+
+};
+
+CLICK_ENDDECLS
+#endif
