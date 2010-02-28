171b577cd0ff3253ab106e140e1353a31642e33e
gowricp <gowricp@7ed3ea50-16bc-11df-9d3e-97c80f6cbf9d>
2010-02-28 01:43:58 +0000

    header for publishaccessinfo

---
+++ veil/publishaccessinfo.hh
@@ -0,0 +1,33 @@
+#ifndef CLICK_VEILPUBLISHACCESSINFO_HH
+#define CLICK_VEILPUBLISHACCESSINFO_HH
+#include <click/element.hh>
+#include <click/timer.hh>
+#include "utilities.hh"
+#include "click_veil.hh"
+#include "hosttable.hh"
+
+CLICK_DECLS
+ 
+class VEILPublishAccessInfo : public Element {
+	public:
+ 
+		VEILPublishAccessInfo();
+		~VEILPublishAccessInfo();
+ 
+		const char *class_name() const { return "VEILPublishAccessInfo"; }
+		const char *port_count() const { return PORTS_0_1; }
+		const char *processing() const { return PUSH; }
+ 
+		int configure(Vector<String> &, ErrorHandler *);
+		int initialize(ErrorHandler *);
+
+		void run_timer(Timer *);
+
+	private:
+		Timer myTimer;
+		VEILHostTable *hosts;
+
+};
+ 
+CLICK_ENDDECLS
+#endif
