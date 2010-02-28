11b9bd8cf8f25f86227920b2fb38071ff69412f3
gowricp <gowricp@7ed3ea50-16bc-11df-9d3e-97c80f6cbf9d>
2010-02-28 01:29:37 +0000

    added constants and struct access_info

--- veil/click_veil.hh
+++ veil/click_veil.hh
@@ -2,11 +2,11 @@
 #define CLICK_VEIL_HH
  
 #include <click/etheraddress.hh>
+#include <click/ipaddress.hh>
 #include <click/string.hh>
 #include <clicknet/ip.h>
 #include <clicknet/ether.h>
 #include <clicknet/udp.h>
-
 #include <click/vid.hh>
 
 CLICK_DECLS
@@ -15,22 +15,32 @@ CLICK_DECLS
 #define ETHERTYPE_VEIL          0x1234
 
 #define VEIL_HELLO 		0x01	
-#define VEIL_PUBLISH		0x02	
-#define VEIL_QUERY		0x03	
-#define VEIL_RDVREPLY		0x04	
+#define VEIL_RDV_PUBLISH	0x02	
+#define VEIL_RDV_QUERY		0x03	
+#define VEIL_RDV_REPLY		0x04	
 #define VEIL_ARP_REQ		0x05	
 #define VEIL_ARP_RPLY		0x06
+#define VEIL_PUBLISH		0x07
+#define VEIL_QUERY		0x08
 
 // Intervals are in ms
 #define VEIL_HELLO_INTERVAL		20000
-#define VEIL_PUBLISH_INTERVAL		20000
+#define VEIL_PUBLISH_INTERVAL		50000
+#define VEIL_RDV_INTERVAL		60000
 /* value greater than hello/publish interval so entries don't expire just    
- * when hello/publish pkts need to be resent. risking temp invalid routes. 
+ * when hello/publish pkts need to be resent. risking temporarily invalid    
+ * routes. 
  */ 
 #define VEIL_TBL_ENTRY_EXPIRY		21000
 
+
 struct veil_header {
 	uint16_t packetType;
+};
+
+struct access_info{
+	IPAddress ip;
+	VID vid;
 };
 
 CLICK_ENDDECLS
