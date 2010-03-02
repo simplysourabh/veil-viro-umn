00905b3b9de1b57838d5d90ed25cdeced629b0a8
gowricp <gowricp@7ed3ea50-16bc-11df-9d3e-97c80f6cbf9d>
2010-03-02 14:54:09 +0000

    header for processaccessinfo

---
+++ veil/processaccessinfo.hh
@@ -0,0 +1,46 @@
+#ifndef CLICK_VEILPROCESSACCESSINFO_HH
+#define CLICK_VEILPROCESSACCESSINFO_HH
+
+#include <click/element.hh>
+#include <click/timer.hh>
+#include <click/hashmap.hh>
+#include "utilities.hh"
+#include "click_veil.hh"
+#include "mappingtable.hh"
+#include "interfacetable.hh"
+
+CLICK_DECLS
+
+class VEILProcessAccessInfo : public Element {
+	public:
+		VEILProcessAccessInfo();
+		~VEILProcessAccessInfo();
+
+		const char* class_name() const { return "VEILProcessAccessInfo"; }
+		const char* port_count() const { return PORTS_1_1; }
+		const char* processing() const { return PUSH; }
+
+		int configure(Vector<String>&, ErrorHandler*);
+		Packet* smaction(Packet*);
+	 	virtual void push (int, Packet*);		
+
+	private:
+		VEILInterfaceTable *interfaces;
+		VEILMappingTable *map;
+			
+};
+
+CLICK_ENDDECLS
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
