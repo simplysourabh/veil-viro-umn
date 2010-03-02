50825db673ac4f969146b869640bef96ae54dca0
gowricp <gowricp@7ed3ea50-16bc-11df-9d3e-97c80f6cbf9d>
2010-03-02 15:01:38 +0000

    added and modified annotation macros

--- veil/utilities.hh
+++ veil/utilities.hh
@@ -5,20 +5,25 @@
 #include <click/vid.hh>
 #include <click/ipaddress.hh>
 #include <click/packet_anno.hh>
+#include <click/packet.hh>
 #include "click_veil.hh"
 
 CLICK_DECLS
 
 //this is for access and rdv point rerouting
 #define REROUTE_ANNO_OFFSET       20
-#define REROUTE_ANNO_SIZE         1
-#define REROUTE_ANNO(p)           ((p)->user_anno_c(REROUTE_ANNO_OFFSET))
-#define SET_REROUTE_ANNO(p, v)    ((p)->set_user_anno_c(REROUTE_ANNO_OFFSET, (v)))
+#define REROUTE_ANNO(p)           ((p)->anno_u8(Packet::user_anno_offset + REROUTE_ANNO_OFFSET))
+#define SET_REROUTE_ANNO(p, v)    ((p)->set_anno_u8(Packet::user_anno_offset + REROUTE_ANNO_OFFSET, (v)))
+
+//set port number annotation for all incoming packets
+#define PORT_ANNO_OFFSET          10
+#define PORT_ANNO(p)              ((p)->anno_u32(Packet::user_anno_offset + PORT_ANNO_OFFSET))
+#define SET_PORT_ANNO(p, v)       ((p)->set_anno_u32(Packet::user_anno_offset + PORT_ANNO_OFFSET, (v)))
 
 //TODO: find a good hash function. 
 //shouldn't get an all 0's vid or something equally crazy
 //we don't want hash clashes either
-VID calculate_access_switch(IPAddress *ip){
+static VID calculate_access_switch(IPAddress *ip){
 	unsigned char* data = ip->data();
 	unsigned char val[VID_LEN];
 	memset (val, 0, sizeof(val));	
