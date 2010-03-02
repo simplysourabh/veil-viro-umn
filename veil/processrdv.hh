d46c40b7eff67f08dcea148fa873be1eae0f0a6a
gowricp <gowricp@7ed3ea50-16bc-11df-9d3e-97c80f6cbf9d>
2010-03-02 14:56:09 +0000

    header for processrdv

---
+++ veil/processrdv.hh
@@ -0,0 +1,34 @@
+#ifndef CLICK_VEILPROCESSRDV_HH
+#define CLICK_VEILPROCESSRDV_HH
+#include <click/element.hh>
+#include <click/timer.hh>
+#include <click/hashmap.hh>
+#include "rendezvoustable.hh"
+#include "routetable.hh"
+#include "interfacetable.hh"
+
+CLICK_DECLS
+
+class VEILProcessRDV: public Element {
+	public:
+		VEILProcessRDV();
+		~VEILProcessRDV();
+
+		const char* class_name() const { return "VEILProcessRDV"; }
+		const char* port_count() const { return PORTS_1_1; } 
+		const char* processing() const { return "a/h"; }
+
+		int configure(Vector<String>&, ErrorHandler*);
+		Packet* smaction(Packet*);
+	 	virtual void push (int, Packet*);
+
+	private:
+
+		VEILRouteTable *routes;
+		VEILRendezvousTable *rdvs;
+		VEILInterfaceTable *interfaces;
+			
+};
+
+CLICK_ENDDECLS
+#endif
