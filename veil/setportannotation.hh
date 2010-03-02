ced10983ae1e65900bf862878e8acf5813771dd5
gowricp <gowricp@7ed3ea50-16bc-11df-9d3e-97c80f6cbf9d>
2010-03-02 15:00:27 +0000

    header for setportannotation

---
+++ veil/setportannotation.hh
@@ -0,0 +1,26 @@
+#ifndef CLICK_VEILSETPORTANNOTATION_HH
+#define CLICK_VEILSETPORTANNOTATION_HH
+#include <click/element.hh>
+ 
+CLICK_DECLS
+ 
+class VEILSetPortAnnotation : public Element {
+	public:
+ 
+		VEILSetPortAnnotation();
+		~VEILSetPortAnnotation();
+ 
+		const char *class_name() const { return "VEILSetPortAnnotation"; }
+		const char *port_count() const { return PORTS_1_1; }
+		const char *processing() const { return "a/h"; }
+ 
+		int configure(Vector<String> &, ErrorHandler *);
+		virtual void push(int, Packet*);		
+
+	private:
+		int port;
+
+};
+ 
+CLICK_ENDDECLS
+#endif
