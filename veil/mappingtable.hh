42f548bb40f2a840bd7f2529686590e75c49eff7
gowricp <gowricp@7ed3ea50-16bc-11df-9d3e-97c80f6cbf9d>
2010-03-03 10:23:44 +0000

    no significant changes

--- veil/mappingtable.hh
+++ veil/mappingtable.hh
@@ -15,7 +15,7 @@ class VEILMappingTable : public Element {
 		VEILMappingTable();
 		~VEILMappingTable();
 
-		const char* class_name() const { return "VEILIPMappingTable"; }
+		const char* class_name() const { return "VEILMappingTable"; }
 		const char* port_count() const { return PORTS_0_0; }
 		const char* processing() const { return PUSH; }
 
