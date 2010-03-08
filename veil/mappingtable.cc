87e2819e904dcfabab944c874df8f3d70d07ea87
gowricp <gowricp@7ed3ea50-16bc-11df-9d3e-97c80f6cbf9d>
2010-03-08 01:32:11 +0000

    no significant changes

--- veil/mappingtable.cc
+++ veil/mappingtable.cc
@@ -34,9 +34,11 @@ VEILMappingTable::cp_mapping(String s, ErrorHandler *errh)
 int
 VEILMappingTable::configure(Vector<String> &conf, ErrorHandler *errh)
 {
+	int res = -1;
 	for (int i = 0; i < conf.size(); i++) {
-		cp_mapping(conf[i], errh);
+		res = cp_mapping(conf[i], errh);
 	}
+	return res;
 }
 
 void
