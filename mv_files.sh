1486980dd090249b53f19d9d54e66f547f02a656
simply.sourabh <simply.sourabh@7ed3ea50-16bc-11df-9d3e-97c80f6cbf9d>
2010-12-28 19:15:26 +0000

    slightly modified

--- mv_files.sh
+++ mv_files.sh
@@ -2,41 +2,42 @@
 cd ~/veil-viro-umn
 
 # now copy the vid.hh to include/click
-cp vid.hh /opt/ns-allinone-2.29/click/include/click/
+cp vid.hh /opt/click/include/click/
 
 
 # copy confparse.cc to click/lib
-#cp confparse.cc /opt/ns-allinone-2.29/click/lib/confparse.cc
+cp confparse.cc /opt/click/lib/confparse.cc
 
 # copy confparse.hh to include/click/
-#cp confparse.hh /opt/ns-allinone-2.29/click/include/click/confparse.hh 
+cp confparse.hh /opt/click/include/click/confparse.hh 
 
 # copy veil directory to click/elements/
-cp veil/*.hh /opt/ns-allinone-2.29/click/elements/veil/ 
-cp veil/*.cc /opt/ns-allinone-2.29/click/elements/veil/
+mkdir /opt/click/elements/veil
+cp veil/*.hh /opt/click/elements/veil/ 
+cp veil/*.cc /opt/click/elements/veil/
 
 
 # now copy the configure.ac and Makefile.in files too.
-#cp configure.ac /opt/ns-allinone-2.29/click/elements/veil/
-#cp Makefile.in /opt/ns-allinone-2.29/click/elements/veil/
+cp configure.ac /opt/click/elements/veil/
+cp Makefile.in /opt/click/elements/veil/
 
-cd /opt/ns-allinone-2.29/click/elements/veil
+cd /opt/click/elements/veil
 #sudo ./configure
 #sudo make elemlist
 #sudo make 
 #sudo make install 
 
 # now go the click root directory run configure, make, make install
-cd /opt/ns-allinone-2.29/click/
-#sudo ./configure  --enable-analysis --enable-test --enable-userlevel --enable-local
-#sudo make elemlist
+cd /opt/click/
+sudo ./configure  --enable-analysis --enable-test --enable-userlevel --enable-local
+sudo make elemlist
 sudo make 
 sudo make install
 
 # go to veil directory and run configure, make and make install
 cd elements/veil
-#sudo ./configure
-#sudo make elemlist
+sudo ./configure
+sudo make elemlist
 sudo make 
 sudo make install 
 
--- test-scripts/test_processip.click
+++ test-scripts/test_processip.click
@@ -1,4 +1,4 @@
-require(veil);
+//require(veil);
 
 hosts::VEILHostTable(5654642a0001 22:23:ae:89:90:de 192.168.3.5, 
 		     934764da0001 12:23:ae:56:90:de 192.169.3.5);
