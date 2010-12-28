9b5e87ecc54436fd80270c2ffcd04bba27e6eb00
simply.sourabh <simply.sourabh@7ed3ea50-16bc-11df-9d3e-97c80f6cbf9d>
2010-12-28 19:18:02 +0000

    adding the quickinstall and the install scripts

---
+++ install.sh
@@ -0,0 +1,40 @@
+## install git, vim, g++
+sudo apt-get install git
+sudo apt-get install vim
+sudo apt-get install g++
+
+## download click
+cd /opt
+git clone git://read.cs.ucla.edu/click click
+
+## download veil
+cd ~
+svn checkout https://veil-viro-umn.googlecode.com/svn/ veil-viro-umn --username simply.sourabh
+
+
+## copy the veil files.
+## go to the directory where I downloaded the files from the svn
+cd ~/veil-viro-umn
+## now copy the vid.hh to include/click
+cp vid.hh /opt/click/include/click/
+## copy confparse.cc to click/lib
+cp confparse.cc /opt/click/lib/confparse.cc
+## copy confparse.hh to include/click/
+cp confparse.hh /opt/click/include/click/confparse.hh 
+## copy veil directory to click/elements/
+mkdir /opt/click/elements/veil
+cp veil/*.hh /opt/click/elements/veil/ 
+cp veil/*.cc /opt/click/elements/veil/
+## now copy the configure.ac and Makefile.in files too.
+cp configure.ac /opt/click/elements/veil/
+cp Makefile.in /opt/click/elements/veil/
+
+cd /opt/click/
+## IMPORTANT: edit the ./configure and add the option "--enable-veil" below the "--enable-local" option
+sudo ./configure  --enable-analysis --enable-test --enable-userlevel --enable-local --enable-veil
+sudo make elemlist
+sudo make 
+sudo make install
+
+cd ~/veil-viro-umn 
+#
