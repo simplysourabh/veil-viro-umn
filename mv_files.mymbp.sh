b3c73d2c561e9be484a1a43320db8cfd1ec16323
simply.sourabh <simply.sourabh@7ed3ea50-16bc-11df-9d3e-97c80f6cbf9d>
2011-01-25 19:44:10 +0000

    Added a new mv_file script for the vms running on my macbook

---
+++ mv_files.mymbp.sh
@@ -0,0 +1,45 @@
+# go to the directory where I downloaded the files from the svn
+cd ~/veil-viro-umn
+
+# now copy the vid.hh to include/click
+#cp vid.hh /opt/ns-allinone-2.29/click/include/click/
+
+
+# copy confparse.cc to click/lib
+#cp confparse.cc /opt/ns-allinone-2.29/click/lib/confparse.cc
+
+# copy confparse.hh to include/click/
+#cp confparse.hh /opt/ns-allinone-2.29/click/include/click/confparse.hh 
+
+# copy veil directory to click/elements/
+#mkdir /opt/ns-allinone-2.29/click/elements/veil
+cp veil/*.hh /opt/ns-allinone-2.29/click/elements/veil/ 
+cp veil/*.cc /opt/ns-allinone-2.29/click/elements/veil/
+
+
+# now copy the configure.ac and Makefile.in files too.
+#cp configure.ac /opt/ns-allinone-2.29/click/elements/veil/
+#cp Makefile.in /opt/ns-allinone-2.29/click/elements/veil/
+
+cd /opt/ns-allinone-2.29/click/elements/veil
+#sudo ./configure
+#sudo make elemlist
+#sudo make 
+#sudo make install 
+
+# now go the click root directory run configure, make, make install
+cd /opt/ns-allinone-2.29/click/
+#sudo ./configure  --enable-analysis --enable-test --enable-userlevel --enable-local
+#sudo make elemlist
+sudo make 
+sudo make install
+
+# go to veil directory and run configure, make and make install
+cd elements/veil
+#sudo ./configure
+#sudo make elemlist
+sudo make 
+sudo make install 
+
+cd ~/veil-viro-umn 
+#
